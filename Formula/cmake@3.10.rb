3db745a6ee9642b9e91105b1954161223ccd5310
WU Qi <wilson.over.cloud@gmail.com>
2018-08-04 13:09:07 -0500

    add more cmake versions

---
+++ Formula/cmake@3.10.rb
@@ -0,0 +1,65 @@
+class CmakeAT310 < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  url "https://cmake.org/files/v3.10/cmake-3.10.3.tar.gz"
+  sha256 "0c3a1dcf0be03e40cf4f341dda79c96ffb6c35ae35f2f911845b72dab3559cf8"
+  head "https://cmake.org/cmake.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c7c019369acca87aaeb80fe64bf6599bed077c185bc8d1f8978ed55bcb803a42" => :high_sierra
+    sha256 "dcf1a904a115bddc61dc0f21f156e600d9950147f6db20d23ce376c145dc470a" => :sierra
+    sha256 "8a71e913491f5753e679f15d86c445abd547ff90633df164148231afdb98b089" => :el_capitan
+  end
+
+  option "without-docs", "Don't build man pages"
+  option "with-completion", "Install Bash completion (Has potential problems with system bash)"
+
+  depends_on "sphinx-doc" => :build if build.with? "docs"
+
+  # The `with-qt` GUI option was removed due to circular dependencies if
+  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
+  # For the GUI application please instead use `brew cask install cmake`.
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11 if MacOS.version < :mavericks
+
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    if build.with? "docs"
+      # There is an existing issue around macOS & Python locale setting
+      # See https://bugs.python.org/issue18378#msg215215 for explanation
+      ENV["LC_ALL"] = "en_US.UTF-8"
+      args << "--sphinx-man" << "--sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
+    end
+
+    system "./bootstrap", *args, "--", "-DCMAKE_BUILD_TYPE=Release"
+    system "make"
+    system "make", "install"
+
+    if build.with? "completion"
+      cd "Auxiliary/bash-completion/" do
+        bash_completion.install "ctest", "cmake", "cpack"
+      end
+    end
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
